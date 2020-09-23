580651702c835740b1cceceb6d62f6d4fd3d16d9
DanL1L <39854697+DanL1L@users.noreply.github.com>
2020-09-23 20:12:51 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    float x,y,p,V,Z;
+    printf("\t\tProblema 9\n");
+    printf("\t------------------------------\n");
+    printf("\t Introduceti valoarea x:");
+    scanf("%f",&x);
+    printf("\t Introduceti valoarea y:");
+    scanf("%f",&y);
+    V=log(y+0.95)+sin(pow(x,4));
+    printf("\t Rezultatul functiei V: %.2f\n",V);
+    printf("\t------------------------------\n");
+    printf("\n");
+    printf("\t\tProblema 26\n");
+    printf("\t------------------------------\n");
+    printf("\t Introduceti valoarea p:");
+    scanf("%f",&p);
+    p=pow(p,2);
+    Z=sin(pow(p+0.4,3));
+    printf("\t Rezultatul functiei Z: %.2f\n",Z);
+    printf("\t------------------------------\n");
+    printf("\n");
+    printf("\t\tProblema 20\n");
+    printf("\t------------------------------\n");
+    float m,t,l,K_0,K_1;
+    printf("\t Introduceti valoarea m echivalant 'y':");
+    scanf("%f",&m);
+    printf("\t Introduceti valoarea t:");
+    scanf("%f",&t);
+    printf("\t Introduceti valoarea l:");
+    scanf("%f",&l);
+    t=pow(t,2);
+    K_0=2*t+3*l+7.2;
+    K_1=K_0/log(m+exp(2*l));
+    printf("\t Rezultatul functiei K: %.2f\n",K_1);
+    printf("\t------------------------------\n");
+    return 0;
+}
